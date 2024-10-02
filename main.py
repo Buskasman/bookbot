fe5ffef5867fe8c188ab6a83d1db173bc535cfce
Buskas <buskasman@hotmail.com>
2024-10-02 21:19:21 +0200

    All done

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ def main():
     text = get_book_text(book_path)
     word_number = word_count(text)
     letters_count = get_num_letters(text)
-    print(f"Letter count is as follows {letters_count}")
-    
+    letters_sorted = sort_dict(letters_count)
+    print(f"----- Begin of bookreport on {book_path} -----")
+    print(f"{word_number} number of word were found in this document")
+    for i in letters_sorted:
+        print(f"The letter {i} appears {letters_sorted.get(i)} times in the document")
+    print("--- End of report ---")
+
+      
+        
+        
+
+def sort_dict(dict):
+    sorted_list = sorted(dict.items(), key = lambda x: x[1], reverse=True)       
+    sorted_dict ={}
+    for i in sorted_list:
+        x = str(i).replace("(", "").replace(")", "").replace(",", "")
+        y = x.split()
+        sorted_dict[y[0]] = y[1]
+    return sorted_dict
+
+
 
 def get_num_letters(text):
     low_case = text.lower()
@@ -15,10 +34,6 @@ def get_num_letters(text):
         count = split_list.count(letter)
         letter_count.update({letter: count})
     return letter_count
-        
-    
-
-
 
 def word_count(text):
     split_text = text.split()
